23fd989453cc5a5582228fdd4f40d5438f489903
infiniator <infiniator@gmail.com>
2017-11-30 01:33:35 +0530

    Finished the fitness function

--- src/chromosome.py
+++ src/chromosome.py
@@ -1,22 +1,17 @@
 from functools import cmp_to_key
-
 from src.dataset import readData
 from src.comparator import compare
 
 
-class Chromosome:  # done
-    schedule = []        # list of numProcs lists having at most numTasks tasks
-    numTasks = 0         # number of input tasks
-    numProcs = 0         # number of input processors
-    fitness = 0          # fitness of the chromosome
+class Chromosome:
+    schedule = []  # list of numProcs lists having at most numTasks tasks
+    numTasks = 0  # number of input tasks
+    numProcs = 0  # number of input processors
+    fitness = 0  # fitness of the chromosome
     initialized = False  # used to ensure readData() is called only once
-    data = None          # data from the Kasahara dataset
+    data = None  # data from the Kasahara dataset
 
     def __init__(self):
-        pass
-
-    # the fitness of a chromosome, currently, is its finishing time
-    def calculateFitness(self):
         if not Chromosome.initialized:
             Chromosome.initialized = True
             Chromosome.data = readData(0)
@@ -31,5 +26,26 @@ class Chromosome:  # done
             for i in Chromosome.data:  # save key for internal calculation later
                 i['key'] = j
                 j += 1
-        # for i in Chromosome.data:
-            # calculation of fitness is incomplete
+
+    # the fitness of a chromosome, currently, is its finishing time
+    def calculateFitness(self):
+        ftp = [0] * Chromosome.numProcs
+        for i in range(0, Chromosome.numTasks):
+            p = self.searchInProc(i)
+            ftp[p] += Chromosome.data[i]['procTime']
+            for j in range(0, Chromosome.numProcs):
+                if p == j:
+                    continue
+                for k in self.schedule[j]:
+                    temp = [k[i] for i in k if 'pre' in i]
+                    if len(temp) > 0 and ftp[p] < ftp[j]:
+                        ftp[p] = ftp[j] + Chromosome.data[i]['procTime']
+                        break
+        return max(ftp)
+
+    # to internally search for concerned processor of a task
+    def searchInProc(self, task):
+        for i in self.schedule:
+            for j in i:
+                if (task + 1) == self.schedule[i][j]['procID']:
+                    return i
